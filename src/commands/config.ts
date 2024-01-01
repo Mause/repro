e5b2a83c59001bfbbc4bd9c7eb7c5ad4dbb91d30
Elliana May <me@mause.me>
2024-01-02 00:30:05 +0800

    add config cmd

---
+++ src/commands/config.ts
@@ -0,0 +1,15 @@
+import { Args, Command } from "@oclif/core";
+
+export default class Config extends Command {
+  static description = "Dumps current config";
+
+  async run(): Promise<void> {
+    console.log("Config");
+    console.log("------");
+    console.log(this
+                .config
+
+                .configDir);
+                console.log(this.config.theme);
+  }
+}
