02bf940ad138efcf08987edb1e28e710655f0fa4
Elliana May <me@mause.me>
2024-01-02 13:45:37 +0800

    feat: add ability to reset config

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,14 +1,53 @@
-import { Command, ux } from "@oclif/core";
+import { Command, ux, Flags } from "@oclif/core";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+
+async function writeJsonFile(file: string, data: any): Promise<void> {
+  await fs.writeFile(file, JSON.stringify(data, undefined, 2));
+}
+
+const DEFAULT = {
+  bin: "white",
+  command: "cyan",
+  commandSummary: "white",
+  dollarSign: "white",
+  flag: "white",
+  flagDefaultValue: "blue",
+  flagOptions: "white",
+  flagRequired: "red",
+  flagSeparator: "white",
+  sectionDescription: "white",
+  sectionHeader: "underline",
+  topic: "white",
+  version: "white",
+};
 
 export default class Config extends Command {
   static description = "Dumps current config";
 
+  static flags = {
+    "default-config": Flags.boolean({
+      type: "boolean" as const,
+      default: false,
+      description: "Show default config",
+      name: "default",
+    }),
+  };
+
   async run(): Promise<void> {
     this.log("Config");
     this.log("------");
-    this.log(this.config.configDir);
+    const { configDir, theme } = this.config;
+    this.log(`Config dir: ${configDir}`);
+    const themePath = path.join(configDir, "theme.json");
     this.log("------");
-    this.log(JSON.stringify(this.config.theme, undefined, 2));
+    this.log("Theme: " + JSON.stringify(theme, undefined, 2));
+
+    const { flags } = await this.parse(Config);
+
+    if (flags["default-config"]) {
+      await writeJsonFile(themePath, DEFAULT);
+    }
   }
 
   public info(msg: string): void {
