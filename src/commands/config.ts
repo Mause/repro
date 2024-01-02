3d016440c4cb972979cbb61f813b82c95980d0ed
Elliana May <me@mause.me>
2024-01-02 12:17:27 +0800

    use colours

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,13 +1,17 @@
-import { Command } from "@oclif/core";
+import { Command, ux } from "@oclif/core";
 
 export default class Config extends Command {
   static description = "Dumps current config";
 
   async run(): Promise<void> {
-    console.log("Config");
-    console.log("------");
-    console.log(this.config.configDir);
-    console.log("------");
-    console.log(this.config.theme);
+    this.log("Config");
+    this.log("------");
+    this.log(this.config.configDir);
+    this.log("------");
+    this.log(JSON.stringify(this.config.theme, undefined, 2));
+  }
+
+  public info(msg: string): void {
+    this.log(ux.colorize(this.config.theme?.info, "info:"), msg);
   }
 }
